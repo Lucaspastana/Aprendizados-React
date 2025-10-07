2090d5115bd0d59630812ad5437844c6e3686111
LucasPastana <lucasamorimpast@gmail.com>
2025-10-06 23:42:55 -0300

    Cond

--- Aula02-UseState/UseState/Start-hook/src/assets/components/ConditionalRender.jsx
+++ Aula02-UseState/UseState/Start-hook/src/assets/components/ConditionalRender.jsx
@@ -2,11 +2,23 @@ import React from "react";
 //Renderização condicional em React funciona da mesma forma que condições funcionam em JavaScript. Use operadores de JavaScript como if ou operador condicional para criar elementos representando o estado atual, e deixe o React atualizar a UI para corresponde-los.
 const ConditionalRender=()=>{
   const x =false;
+  const name =  'lucas'
 
   return(
     <div>
       <h3>Isso sera exibido ?</h3>
       {x && <p>Se X for true simm </p>}
+      <h3>TERNARIOS</h3>
+      {name == 'lucas' ? (
+        <div>
+          <p>Sim</p>
+        </div>
+      ):(
+        <div>
+          <p>não</p>
+        </div>
+
+      )}
     </div>
   )
 }
